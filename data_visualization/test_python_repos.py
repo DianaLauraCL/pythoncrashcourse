52314b3897444264567097c8435a313186a20ded
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-22 10:27:22 -0600

    Testing 'python_repos'

--- data_visualization/test_python_repos.py
+++ data_visualization/test_python_repos.py
@@ -1,12 +1,10 @@
 import python_repos
 import requests
 
-url = 'http://api.github.com/search/repositories'
-url += '?q=language:python+sort:stars+stars:>10000'
-
-headers = {'Accept': 'application/vnd.github.v3+json'}
-r = requests.get(url, headers=headers)
-# print(f"Status code: {r.status_code}")
 def test_python_repos():
+    url = 'http://api.github.com/search/repositories'
+    url += '?q=language:python+sort:stars+stars:>10000'
+    r = requests.get(url)
+
     status_code = r.status_code
     assert status_code == 200
