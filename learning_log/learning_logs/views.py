71e71e7a6b316350de0e8dca411c82443a446511
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2024-12-29 12:53:08 -0600

    Protecting a User's Topics

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 
 def index(request):
     """The home page for Learning Log"""
@@ -19,6 +20,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
