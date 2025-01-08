efb41c808f84fc18ba676850ef270ad1f5f609b7
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2025-01-08 14:16:22 -0600

    Displaying the level

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -81,6 +81,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
 
             # Hide the mouse cursor.
@@ -147,6 +148,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increse_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update position"""
