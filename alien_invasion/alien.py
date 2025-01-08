b534a85530e5dfb801a52e5c02fcfc7b5adfa9f4
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2025-01-07 18:57:02 -0600

    Fixing update()

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -29,4 +29,4 @@ class Alien(Sprite):
     def update(self):
         """Move the alien right or left."""
         self.x += self.settings.alien_speed * self.settings.fleet_direction
-        self.rect,x = self.x
+        self.rect.x = self.x
