935ee8b94fe1c3be1895e2eb03faf930ea49020e
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2025-01-08 14:32:59 -0600

    Displaying the Number of Ships

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -82,6 +82,7 @@ class AlienInvasion:
             self.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_Ships()
 
 
             # Hide the mouse cursor.
@@ -176,8 +177,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_Ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
