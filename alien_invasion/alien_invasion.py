ad0ee07a99d5914a0a50063b44073715a6c1f5c6
Diana Castro <diana.laura.castro@comunidad.unam.mx>
2025-01-08 11:24:42 -0600

    Drawing the Button to the Screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 from python_work.aliens import new_alien
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -37,6 +38,9 @@ class AlienInvasion:
 
         # Start Alien Invasion in an inactive state.
         self.game_active = False
+
+        # Make the Play button.
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -194,6 +198,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button ig the game is inactive.
+        if not self.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
